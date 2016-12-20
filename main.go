de75ed5760492a0058e82eba0341c71c586e8b42
Vladimir Bauer <vbauerster@gmail.com>
2016-12-20 23:38:33 +0500

    withContext

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"flag"
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"regexp"
 	"strings"
+	"sync"
 
 	"github.com/pkg/errors"
+	"github.com/vbauerster/mpb"
 )
 
 const maxRedirects = 10
@@ -25,17 +30,38 @@ func init() {
 }
 
 func main() {
-	flag.Parse()
 	url2 := "https://homebrew.bintray.com/bottles/libtiff-4.0.7.sierra.bottle.tar.gz"
 
 	actualLocation, err := follow(parseURL(url2))
 	if err != nil {
 		log.Fatal(err)
 	}
-	if actualLocation.StatusCode != http.StatusOK {
-		log.Fatalf("non-200 status: %s", actualLocation.StatusCode)
+	// fmt.Printf("actualLocation = %+v\n", actualLocation)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	if actualLocation.AcceptRanges == "bytes" && actualLocation.StatusCode == 206 {
+		var wg sync.WaitGroup
+		p := mpb.New().SetWidth(60)
+		partSize := actualLocation.ContentLength / 2
+		actualLocation.Parts = make(map[int]Part)
+		for i := int64(0); i < 2; i++ {
+			offset := i * partSize
+			start, stop := offset, offset+partSize-1
+			part := Part{start, stop}
+			actualLocation.Parts[int(i)] = part
+			wg.Add(1)
+			go downloadPart(ctx, &wg, p, actualLocation, start, stop, i)
+		}
+		wg.Wait()
+		p.Stop()
 	}
-	fmt.Printf("actualLocation = %+v\n", actualLocation)
+
+	data, err := json.MarshalIndent(actualLocation, "", "	")
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
 }
 
 type ActualLocation struct {
@@ -45,6 +71,58 @@ type ActualLocation struct {
 	AcceptRanges      string
 	StatusCode        int
 	ContentLength     int64
+	Parts             map[int]Part
+}
+
+type Part struct {
+	Start, Stop int64
+}
+
+func downloadPart(ctx context.Context, wg *sync.WaitGroup, p *mpb.Progress, al *ActualLocation, start, stop, n int64) {
+	defer wg.Done()
+	name := fmt.Sprintf("part#%d: ", n)
+	req, err := http.NewRequest(http.MethodGet, al.Location, nil)
+	if err != nil {
+		log.Printf("%s%v", name, err)
+		return
+	}
+	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, stop))
+
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		log.Printf("%s%v", name, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	partname := "temp"
+	if al.SuggestedFileName != "" {
+		partname = al.SuggestedFileName
+	}
+	partname = fmt.Sprintf("%s.part%d", partname, n)
+
+	dest, err := os.Create(partname)
+	if err != nil {
+		log.Printf("%s%v", name, err)
+		return
+	}
+
+	bar := p.AddBar(int(stop-start)+1).
+		PrependName(name, len(name)).
+		PrependCounters(mpb.UnitBytes, 20).
+		AppendETA(-6)
+
+	// create proxy reader
+	reader := bar.ProxyReader(resp.Body)
+	// and copy from reader
+	_, err = io.Copy(dest, reader)
+
+	if closeErr := dest.Close(); err == nil {
+		err = closeErr
+	}
+	if err != nil {
+		log.Printf("%s: %v", name, err)
+	}
 }
 
 func follow(url *url.URL) (*ActualLocation, error) {
@@ -89,7 +167,12 @@ func follow(url *url.URL) (*ActualLocation, error) {
 }
 
 func getResp(client *http.Client, url string) (*http.Response, error) {
-	resp, err := client.Get(url)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot make request with %q", url)
+	}
+	req.Header.Set("Range", "bytes=0-")
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, errors.Wrapf(err, "cannot get %q", url)
 	}
