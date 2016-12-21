a419e2830283645a6a8c0ebfb23d7dd70f13428b
Vladimir Bauer <vbauerster@gmail.com>
2016-12-21 21:32:56 +0500

    part.download

--- main.go
+++ main.go
@@ -38,23 +38,54 @@ func main() {
 	}
 	// fmt.Printf("actualLocation = %+v\n", actualLocation)
 
+	parts := 2
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	if actualLocation.AcceptRanges == "bytes" && actualLocation.StatusCode == 206 {
 		var wg sync.WaitGroup
-		p := mpb.New().SetWidth(60)
+		pb := mpb.New().SetWidth(60)
 		partSize := actualLocation.ContentLength / 2
 		actualLocation.Parts = make(map[int]Part)
-		for i := int64(0); i < 2; i++ {
-			offset := i * partSize
+		for i := 0; i < parts; i++ {
+			offset := int64(i) * partSize
 			start, stop := offset, offset+partSize-1
-			part := Part{start, stop}
-			actualLocation.Parts[int(i)] = part
+			name := fmt.Sprintf("%s.part%d", actualLocation.SuggestedFileName, i)
+			part := Part{
+				Name:  name,
+				Start: start,
+				Stop:  stop,
+			}
+			actualLocation.Parts[i] = part
 			wg.Add(1)
-			go downloadPart(ctx, &wg, p, actualLocation, start, stop, i)
+			go part.download(ctx, &wg, pb, name, i)
 		}
 		wg.Wait()
-		p.Stop()
+		go func() {
+			pb.Stop()
+		}()
+
+		f, err := os.OpenFile(actualLocation.Parts[0].Name, os.O_APPEND|os.O_WRONLY, 0644)
+		exitOnError(err)
+		defer f.Close()
+
+		buf := make([]byte, 2048)
+		for i := 1; i < parts; i++ {
+			part, err := os.Open(actualLocation.Parts[i].Name)
+			exitOnError(err)
+			for {
+				n, err := part.Read(buf)
+				if err != io.EOF {
+					exitOnError(err)
+				}
+				if n == 0 {
+					break
+				}
+				_, err = f.Write(buf)
+				exitOnError(err)
+			}
+			part.Close()
+		}
+
 	}
 
 	data, err := json.MarshalIndent(actualLocation, "", "	")
@@ -75,42 +106,37 @@ type ActualLocation struct {
 }
 
 type Part struct {
+	Name        string
 	Start, Stop int64
+	Done        bool
 }
 
-func downloadPart(ctx context.Context, wg *sync.WaitGroup, p *mpb.Progress, al *ActualLocation, start, stop, n int64) {
+func (p *Part) download(ctx context.Context, wg *sync.WaitGroup, pb *mpb.Progress, url string, n int) {
 	defer wg.Done()
 	name := fmt.Sprintf("part#%d: ", n)
-	req, err := http.NewRequest(http.MethodGet, al.Location, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		log.Printf("%s%v", name, err)
+		log.Printf("%s%v\n", name, err)
 		return
 	}
-	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, stop))
+	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", p.Start, p.Stop))
 
 	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
 	if err != nil {
-		log.Printf("%s%v", name, err)
+		log.Printf("%s%v\n", name, err)
 		return
 	}
 	defer resp.Body.Close()
 
-	partname := "temp"
-	if al.SuggestedFileName != "" {
-		partname = al.SuggestedFileName
-	}
-	partname = fmt.Sprintf("%s.part%d", partname, n)
-
-	dest, err := os.Create(partname)
+	dest, err := os.Create(p.Name)
 	if err != nil {
-		log.Printf("%s%v", name, err)
+		log.Printf("%s%v\n", name, err)
 		return
 	}
 
-	bar := p.AddBar(int(stop-start)+1).
-		PrependName(name, len(name)).
-		PrependCounters(mpb.UnitBytes, 20).
-		AppendETA(-6)
+	bar := pb.AddBar(int(p.Stop-p.Start)+1).
+		PrependName(name, 0).PrependCounters(mpb.UnitBytes, 20).
+		TrimLeftSpace().AppendETA(-6)
 
 	// create proxy reader
 	reader := bar.ProxyReader(resp.Body)
@@ -118,10 +144,11 @@ func downloadPart(ctx context.Context, wg *sync.WaitGroup, p *mpb.Progress, al *
 	_, err = io.Copy(dest, reader)
 
 	if closeErr := dest.Close(); err == nil {
+		p.Done = true
 		err = closeErr
 	}
 	if err != nil {
-		log.Printf("%s: %v", name, err)
+		log.Printf("%s%v\n", name, err)
 	}
 }
 
@@ -222,4 +249,10 @@ func parseURL(uri string) *url.URL {
 
 func isRedirect(status int) bool {
 	return status > 299 && status < 400
+}
+
+func exitOnError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
