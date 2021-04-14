b29ca0d6f6ded1de81ddcbc830ddd0eb6ff64eea
Vladimir Bauer <vbauerster@gmail.com>
2021-04-15 00:12:40 +0500

    isDone precise check

--- part.go
+++ part.go
@@ -281,7 +281,7 @@ func (p Part) getRange() string {
 }
 
 func (p Part) isDone() bool {
-	return p.Skip || (p.Written > 0 && p.Written > p.Stop-p.Start)
+	return p.Skip || (p.Written > 0 && p.Written == p.total())
 }
 
 func (p Part) total() int64 {
