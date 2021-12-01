9036ab953e68cda926b4e848094878efa4169581
Vladimir Bauer <vbauerster@gmail.com>
2021-12-01 12:53:52 +0500

    refactoring peak decorator
    increase peak window to 1024*128

--- decorators.go
+++ decorators.go
@@ -125,46 +125,46 @@ func (d *mainDecorator) Decor(stat decor.Statistics) string {
 
 type peak struct {
 	decor.WC
-	format        string
-	msg           string
-	byteAcc       int64
-	durAcc        int64
-	minDurPerByte int64
-	once          sync.Once
+	format  string
+	msg     string
+	counter bytesPerDuration
+	min     bytesPerDuration
+	once    sync.Once
+}
+
+type bytesPerDuration struct {
+	bytes    int64
+	duration int64
 }
 
 func newSpeedPeak(format string, wc decor.WC) decor.Decorator {
 	d := &peak{
-		WC:            wc.Init(),
-		format:        format,
-		minDurPerByte: math.MaxInt64,
+		WC:     wc.Init(),
+		format: format,
+		min:    bytesPerDuration{0, math.MaxInt64},
 	}
 	return d
 }
 
 func (s *peak) EwmaUpdate(n int64, dur time.Duration) {
-	s.byteAcc += n
-	s.durAcc += int64(dur)
-	if s.byteAcc > 1024*64 {
-		durPerByte := s.durAcc / s.byteAcc
-		if durPerByte == 0 {
-			return
+	s.counter.bytes += n
+	s.counter.duration += int64(dur)
+	if s.counter.bytes >= 1024*128 {
+		if s.counter.duration < s.min.duration {
+			s.min = s.counter
 		}
-		if durPerByte < s.minDurPerByte {
-			s.minDurPerByte = durPerByte
-		}
-		s.byteAcc, s.durAcc = 0, 0
+		s.counter = bytesPerDuration{0, 0}
 	}
 }
 
 func (s *peak) onComplete() {
-	if s.minDurPerByte == 0 && s.byteAcc != 0 {
-		s.minDurPerByte = s.durAcc / s.byteAcc
+	if s.min.bytes == 0 {
+		s.min = s.counter
 	}
-	if s.minDurPerByte != 0 {
-		s.msg = fmt.Sprintf(s.format, decor.FmtAsSpeed(decor.SizeB1024(1e9/s.minDurPerByte)))
-	} else {
+	if durPerByte := s.min.duration / s.min.bytes; durPerByte == 0 {
 		s.msg = fmt.Sprintf(s.format, decor.FmtAsSpeed(decor.SizeB1024(0)))
+	} else {
+		s.msg = fmt.Sprintf(s.format, decor.FmtAsSpeed(decor.SizeB1024(1e9/durPerByte)))
 	}
 }
 
