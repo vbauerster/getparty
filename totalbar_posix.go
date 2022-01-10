2b51dac1cfa86cdc1d1480b967fc7baaf3adeb1d
Vladimir Bauer <vbauerster@gmail.com>
2022-01-10 23:24:51 +0500

    fix on complete color flash

--- totalbar_posix.go
+++ totalbar_posix.go
@@ -5,5 +5,5 @@ package getparty
 import "github.com/vbauerster/mpb/v7"
 
 func totalBarStyle() mpb.BarFillerBuilder {
-	return mpb.BarStyle().Lbound(" \x1b[36m").Padding("\x1b[0m-").TipOnComplete("=\x1b[0m").Rbound(" ")
+	return mpb.BarStyle().Lbound(" \x1b[36m").Tip(">\x1b[0m").Rbound("\x1b[0m ")
 }
