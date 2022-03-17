578800b952592c883987074c4e3549f362723763
Vladimir Bauer <vbauerster@gmail.com>
2022-03-18 00:39:00 +0500

    narrow moving average a bit

--- decorators.go
+++ decorators.go
@@ -132,7 +132,7 @@ func newSpeedPeak(format string, wc decor.WC) decor.Decorator {
 	d := &peak{
 		WC:     wc.Init(),
 		format: format,
-		mean:   ewma.NewMovingAverage(20),
+		mean:   ewma.NewMovingAverage(17),
 	}
 	return d
 }
