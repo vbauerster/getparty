2f2a55a951dddecd574ea56b25234dd952df6fb5
Vladimir Bauer <vbauerster@gmail.com>
2023-02-18 22:26:27 +0500

    peak ewma 15

--- decorators.go
+++ decorators.go
@@ -129,7 +129,7 @@ func newSpeedPeak(format string, wc decor.WC) decor.Decorator {
 	d := &peak{
 		WC:     wc.Init(),
 		format: format,
-		mean:   ewma.NewMovingAverage(18),
+		mean:   ewma.NewMovingAverage(15),
 	}
 	return d
 }
