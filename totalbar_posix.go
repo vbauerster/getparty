0247b5926c2cfa66fa21b9bbbfa2bc417159c0c9
Vladimir Bauer <vbauerster@gmail.com>
2023-07-17 22:58:14 +0500

    total bar with meta funcs
    posix only

--- totalbar_posix.go
+++ totalbar_posix.go
@@ -5,5 +5,11 @@ package getparty
 import "github.com/vbauerster/mpb/v8"
 
 func totalBarStyle() mpb.BarFillerBuilder {
-	return mpb.BarStyle().Lbound(" \x1b[36m").Padding("\x1b[0m-").Rbound("\x1b[0m ")
+	green := func(s string) string {
+		return "\x1b[32m" + s + "\x1b[0m"
+	}
+	cyan := func(s string) string {
+		return "\x1b[36m" + s + "\x1b[0m"
+	}
+	return mpb.BarStyle().Lbound(" ").Rbound(" ").RefillerMeta(green).FillerMeta(cyan).TipMeta(cyan)
 }
