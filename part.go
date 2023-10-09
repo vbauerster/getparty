07d73e73eccd3d3b625e70efeea238ee5b716307
Vladimir Bauer <vbauerster@gmail.com>
2023-10-09 23:19:14 +0500

    dlogger already ends with space

--- part.go
+++ part.go
@@ -181,7 +181,7 @@ func (p *Part) download(client *http.Client, req *http.Request, timeout, sleep t
 					case p.maxTry:
 						if atomic.LoadUint32(&bar.initialized) == 1 {
 							go bar.Abort(true)
-							fmt.Fprintf(p.progress, "%s %s\n", p.dlogger.Prefix(), ErrMaxRetry)
+							fmt.Fprintf(p.progress, "%s%s\n", p.dlogger.Prefix(), ErrMaxRetry)
 						}
 						retry, err = false, errors.Wrap(ErrMaxRetry, err.Error())
 					}
@@ -213,7 +213,7 @@ func (p *Part) download(client *http.Client, req *http.Request, timeout, sleep t
 			resp, err := client.Do(req.WithContext(ctx))
 			if err != nil {
 				if p.Written == 0 {
-					fmt.Fprintf(p.progress, "%s %s\n", p.dlogger.Prefix(), err.Error())
+					fmt.Fprintf(p.progress, "%s%s\n", p.dlogger.Prefix(), err.Error())
 				} else if atomic.LoadUint32(&bar.initialized) == 0 {
 					if err := p.initBar(&bar, &curTry); err != nil {
 						return false, err
