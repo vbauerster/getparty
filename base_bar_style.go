d3e0230adea0850efd2e00ce007839870e7709ac
Vladimir Bauer <vbauerster@gmail.com>
2024-05-02 01:28:53 +0500

    baseBarStyle: disable tip
    that way refiller looks nicer

--- base_bar_style.go
+++ base_bar_style.go
@@ -3,5 +3,5 @@ package getparty
 import "github.com/vbauerster/mpb/v8"
 
 func baseBarStyle() mpb.BarStyleComposer {
-	return mpb.BarStyle().Lbound(" ").Rbound(" ").Refiller("#").Filler("#").Tip("#")
+	return mpb.BarStyle().Lbound(" ").Rbound(" ").Refiller("#").Filler("#").Tip("")
 }
