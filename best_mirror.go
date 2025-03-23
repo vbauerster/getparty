eab1a745d917440636ed9333c5d577ff8ce9b32a
Vladimir Bauer <vbauerster@gmail.com>
2025-03-23 13:20:48 +0500

    minor: prefer else

--- best_mirror.go
+++ best_mirror.go
@@ -110,9 +110,9 @@ func (cmd Cmd) batchMirrors(input io.Reader, transport http.RoundTripper) mirror
 				err := queryMirror(cmd.Ctx, m, client, timeout, cmd.patcher)
 				if err != nil {
 					cmd.loggers[WARN].Println(err.Error())
-					continue
+				} else {
+					result <- m
 				}
-				result <- m
 			}
 		}()
 	}
