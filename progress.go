9645a62541aa2c1cc02992e9e3cfe286b03dc5a7
Vladimir Bauer <vbauerster@gmail.com>
2025-11-29 02:31:33 +0500

    minor: make total chan after qlen decremented

--- progress.go
+++ progress.go
@@ -87,14 +87,14 @@ func (p *progress) addConcatBar(partCount int) (*mpb.Bar, error) {
 func newProgress(ctx context.Context, session *Session, out, err io.Writer) *progress {
 	var total chan int
 	qlen := len(session.Parts) + 1 // +1 for topBar
-	if !session.Single {
-		total = make(chan int, qlen)
-		qlen += 2 // +2 for total and concat bars
-	}
 	for _, p := range session.Parts {
 		if p.Written != 0 && p.isDone() {
 			qlen--
 		}
+	}
+	if !session.Single {
+		total = make(chan int, qlen)
+		qlen += 2 // +2 for total and concat bars
 	}
 	p := mpb.NewWithContext(ctx,
 		mpb.WithOutput(out),
