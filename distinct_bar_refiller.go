d4f4b21ec70bb03199c5561364b8f37b1a17cd4f
Vladimir Bauer <vbauerster@gmail.com>
2026-04-25 00:39:43 +0500

    refactor: global func green

--- distinct_bar_refiller.go
+++ distinct_bar_refiller.go
@@ -4,9 +4,10 @@ package getparty
 
 import "github.com/vbauerster/mpb/v8"
 
+func green(s string) string {
+	return "\x1b[32m" + s + "\x1b[0m"
+}
+
 func distinctBarRefiller() mpb.BarFiller {
-	green := func(s string) string {
-		return "\x1b[32m" + s + "\x1b[0m"
-	}
 	return baseBarStyle.Refiller("#").RefillerMeta(green).Build()
 }
