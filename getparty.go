vbauerster/getparty

commit 64ee0c73ba13f0f90737381c52278e68533ba863

minor: modern range

getparty.go
@@ -639,7 +639,7 @@ func (m Cmd) follow(patcher httpRequestPatcher, client *http.Client, rawURL stri
 				cancel()
 			}()
 			template := fmt.Sprintf(template, attempt)
-			for i := uint(0); i <= maxRedirect; i++ {
+			for range maxRedirect {
 				m.loggers[INFO].Printf(template, location)
 				m.loggers[DBUG].Printf(template, location)
 

commit 3ca9369b0b0b3ac7497eec61b34b465635eac564

fix: previous commit
Commit 64ee0c73 broke TestMaxRedirect, fix that.

getparty.go
@@ -626,7 +626,7 @@ func (m Cmd) follow(patcher httpRequestPatcher, client *http.Client, rawURL stri
 	location := rawURL
 	timeout := m.getTimeout()
 	template := "GET:R%02d %%s"
-	maxRedirect := cmp.Or(m.opt.MaxRedirect, math.MaxUint)
+	maxRedirect := cmp.Or(m.opt.MaxRedirect, math.MaxUint-1) + 1
 
 	err = backoff.RetryWithContext(m.Ctx, exponential.New(exponential.WithBaseDelay(500*time.Millisecond)),
 		func(attempt uint, _ func()) (bool, error) {